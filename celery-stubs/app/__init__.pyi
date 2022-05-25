ba8bc6d3e7387ffdf175b9a3a58dc53d2053515a
Marti Raudsepp <marti@juffo.org>
2022-05-25 14:38:06 +0300

    Lots of additions

--- celery-stubs/app/__init__.pyi
+++ celery-stubs/app/__init__.pyi
@@ -1,9 +1,14 @@
 from .base import Celery as Celery
 from .utils import AppPickler as AppPickler
 from celery._state import app_or_default as app_or_default, disable_trace as disable_trace, enable_trace as enable_trace, pop_current_task as pop_current_task, push_current_task as push_current_task
-from typing import Any, Optional
+from typing import Any, Optional, TypeVar, Callable, overload
 
-default_app: Any
+F = TypeVar('F', bound=Callable[..., Any])
 
-def bugreport(app: Optional[Any] = ...): ...
-def shared_task(*args: Any, **kwargs: Any): ...
+default_app: Celery
+
+def bugreport(app: Optional[Celery] = ...): ...
+@overload
+def shared_task(__func: F, **kwargs: Any) -> F: ...
+@overload
+def shared_task(*args: Any, **kwargs: Any) -> Callable[[F], F]: ...
