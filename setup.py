fc8f64854531196edeedfc3440ca2f7eebdc7816
Ryan Wang <hwwangwang@gmail.com>
2023-02-10 10:17:44 +0800

    bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def find_stub_files():
 
 
 setup(name='celery-stubs',
-      version='0.1.2',
+      version='0.1.3',
       description=description,
       long_description=install_instructions,
       long_description_content_type='text/markdown',
