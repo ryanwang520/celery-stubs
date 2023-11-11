e935db7e819fe38ad9c102e8a47b276a741045c7
Ryan Wang <hwwangwang@gmail.com>
2023-11-11 19:22:12 +0800

    better typing shared task

--- celery-stubs/__init__.pyi
+++ celery-stubs/__init__.pyi
@@ -1,13 +1,74 @@
-from celery._state import current_app as current_app, current_task as current_task
-from celery.app import shared_task as shared_task
-from celery.app.base import Celery as Celery
-from celery.app.utils import bugreport as bugreport
-from celery.app.task import Task as Task
-from celery.canvas import chain as chain, chord as chord, chunks as chunks, group as group, maybe_signature as maybe_signature, signature as signature, xmap as xmap, xstarmap as xstarmap
-from celery.utils import uuid as uuid
-from collections import namedtuple
+from collections.abc import Callable
+from typing import (
+    Any,
+    Concatenate,
+    Generic,
+    Literal,
+    NotRequired,
+    ParamSpec,
+    TypedDict,
+    TypeVar,
+    overload,
+)
 
-version_info_t = namedtuple('version_info_t', ['major', 'minor', 'micro', 'releaselevel', 'serial'])
+from .result import AsyncResult
 
-# Names in __all__ with no definition:
-#   task
+def __getattr__(name: str) -> Any: ...
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+class Signature:
+    pass
+
+class Request:
+    id: str
+    properties: Any
+
+class TaskGeneric(Generic[P, T]):
+    name: str
+    request: Request
+    def update_state(self, state: Any = ..., meta: Any = ...) -> None: ...
+    def run(self) -> Any: ...
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> T: ...
+    def delay(self, *args: P.args, **kwargs: P.kwargs) -> AsyncResult: ...
+    def apply_async(self, *args: Any, **kwargs: Any) -> AsyncResult: ...
+    def si(self, *args: Any, **kwargs: Any) -> Signature: ...
+    def s(self, *args: Any, **kwargs: Any) -> Signature: ...
+    def retry(
+        self, exc: Exception | None = ..., countdown: int | None = None
+    ) -> Any: ...
+
+class Task(TaskGeneric[Any, Any]):
+    pass
+
+class RetryArgs(TypedDict):
+    max_retries: NotRequired[int]
+
+# Case 1: shared_task is used as a decorator directly without any arguments.
+@overload
+def shared_task(func: Callable[P, T]) -> TaskGeneric[P, T]: ...
+
+# Case 2: shared_task is called with keyword arguments and bind is set to False.
+@overload
+def shared_task(
+    *,
+    name: str | None = None,
+    bind: Literal[False] = False,
+    autoretry_for: tuple[type[Exception], ...] | None = None,
+    base: type[Task] | None = None,
+    default_retry_delay: int | None = None,
+    retry_kwargs: RetryArgs | None = None,
+) -> Callable[[Callable[P, T]], TaskGeneric[P, T]]: ...
+
+# Case 3: shared_task is called with keyword arguments and bind is set to True.
+@overload
+def shared_task(
+    *,
+    name: str | None = None,
+    bind: Literal[True],
+    autoretry_for: tuple[type[Exception], ...] | None = None,
+    base: type[Task] | None = None,
+    default_retry_delay: int | None = None,
+    retry_kwargs: RetryArgs | None = None,
+) -> Callable[[Callable[Concatenate[Task, P], T]], TaskGeneric[P, T]]: ...
--- celery-stubs/signals.pyi
+++ celery-stubs/signals.pyi
@@ -1,28 +1,9 @@
-from typing import Any
+from collections.abc import Callable
+from typing import TypeVar
 
-before_task_publish: Any
-after_task_publish: Any
-task_prerun: Any
-task_postrun: Any
-task_success: Any
-task_retry: Any
-task_failure: Any
-task_revoked: Any
-celeryd_init: Any
-celeryd_after_setup: Any
-worker_init: Any
-worker_process_init: Any
-worker_process_shutdown: Any
-worker_ready: Any
-worker_shutdown: Any
-worker_shutting_down: Any
-heartbeat_sent: Any
-setup_logging: Any
-after_setup_logger: Any
-after_setup_task_logger: Any
-beat_init: Any
-beat_embedded_init: Any
-eventlet_pool_started: Any
-eventlet_pool_preshutdown: Any
-eventlet_pool_postshutdown: Any
-eventlet_pool_apply: Any
+before_task_publish: BeforePublish
+
+Fn = TypeVar("Fn", bound=Callable[..., None])
+
+class BeforePublish:
+    def connect(self, fn: Fn) -> Fn: ...
